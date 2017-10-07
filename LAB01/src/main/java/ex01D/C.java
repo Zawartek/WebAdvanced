0e3cb5f883a0f22261fd98533013ff998272c235
nicosim <nico93simo@hotmail.fr>
2017-10-07 17:04:29 +0200

    ex01D

---
+++ LAB01/src/main/java/ex01D/C.java
@@ -0,0 +1,19 @@
+package ex01D;
+
+public class C {
+  /* void method1(int i, StringBuffer s) */
+  static void method1(int i, StringBuffer s)
+  {
+    i++;
+    s.append("d");
+  }
+  public static void main(String [] args)
+  {
+    int i = 0;
+    StringBuffer s = new StringBuffer("abc");
+    /* methode1(i, s); */
+    method1(i, s);
+    /* System.out.println(i=" + i + ", s=" + s); // i=0, s=abcd */
+    System.out.println("i=" + i + ", s=" + s); // i=0, s=abcd
+  }
+}
